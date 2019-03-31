b3b0c3144dc1099f84ebc2600b7fa9af57f18df8
Xesau <Xesau@users.noreply.github.com>
2019-04-01 00:55:36 +0200

    Add files via upload

---
+++ feed.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+var confToken = 'NTQ5MjEzNTY4NDg3MTI5MDk5.XKFAtQ.TlHAJg0yY3pScK8SMMcDXP42Hwo';
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("guildCreate", guild => {
+    console.log(`Joined server ${guild.id}`);
+});
+
+client.on("messageReactionAdd", (msgReact, user) => {
+    console.log('a');
+    var msg = msgReact.message;
+    
+    if (msgReact.emoji.name == '\u26AA' && msgReact.count == 1) {
+        msg.react('\u26AA');
+        sendMessage(msg.guild, msg.content + "\r\n\r\nhttps://discordapp.com/channels/" + msg.guild.id + "/" + msg.channel.id + "/" + msg.id);
+    }
+    else {
+        console.log(msgReact.emoji.name);
+    }
+});
+
+client.login(confToken);
+
+function sendMessage(server, message) {
+    for(entry of server.channels) {
+        var channel = entry[1];
+        if (channel.type == "text" && channel.name == "feed") {
+            channel.send(message);
+            return;
+        }
+    }
+    console.log("COULDNT FIND #feed IN " + serverId + " (" + client.guilds.get(serverId).name + ")");
+}
